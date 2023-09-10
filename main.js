0cc767910219bd80792806fe4aa02934e211bd65
Joseph Stewart <JosephStewart1752@gmail.com>
2023-09-09 22:12:48 -0400

    Add querySelectors

---
+++ main.js
@@ -0,0 +1,51 @@
+var mainDish = [
+    "Pancakes",
+    "French Toast",
+    "Meatloaf",
+    "BBQ Ribs",
+    "Banana",
+    "Lentil Curry",
+    "Vegetable Stir Fry",
+    "Pizza",
+    "Tacos",
+    "Chalupa"
+]
+
+var side = [
+    "Collard Greens",
+    "Baked Beans",
+    "Asparagus",
+    "Roasted Corn",
+    "Broccoli Salad",
+    "Coleslaw",
+    "Mashed Potatoes",
+    "Bacon",
+    "Cornbread",
+    "Roasted Brussel Sprouts"
+]
+
+var dessert = [
+    "Vanilla Ice Cream",
+    "Chocolate Cake",
+    "Key Lime Pie",
+    "Popsicle",
+    "Pumpkin Cheesecake Roll",
+    "Fudge",
+    "Chocolate Chip Cookies",
+    "Cinnamon Rolls",
+    "Monkey Bread",
+    "Caramel Apple Tart"
+]
+
+var entireMeal =[]
+
+var recipeBtn = document.querySelector('.header__btn')
+var sideBtn = document.querySelector('#side')
+var mainBtn = document.querySelector('#main-dish')
+var dessertBtn = document.querySelector('#dessert')
+var entireBtn = document.querySelector('#entire-meal')
+var cookBtn = document.querySelector('.cook__btn')
+
+var potImage = document.querySelector('.pot-pic')
+var randomText = document.querySelector('.random-text')
+var clearBtn = document.querySelector('.clear__btn')
